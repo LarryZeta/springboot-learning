1e944ec11d2ee6134030757d2d9e779ea6b50ae6
LarryZeta <i@larryzeta.cc>
2019-07-15 21:38:22 +0800

    web update

---
+++ spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/config/MyMvcConfig.java
@@ -0,0 +1,15 @@
+package cc.larryzeta.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        super.addViewControllers(registry);
+        registry.addViewController("/larryzeta").setViewName("success");
+    }
+}
--- spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/controller/HelloController.java
@@ -4,8 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
+
+    @RequestMapping({"/","index.html"})
+    public String index() {
+        return "index";
+    }
 
     @ResponseBody
     @RequestMapping("/hello")
@@ -14,7 +23,9 @@ public class HelloController {
     }
 
     @RequestMapping("/success")
-    public String success() {
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
         return "success";
     }
 
