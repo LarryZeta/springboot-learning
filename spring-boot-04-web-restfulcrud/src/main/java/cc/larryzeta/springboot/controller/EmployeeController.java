26be74df0de19de69660a189543194ff342c2df7
LarryZeta <i@larryzeta.cc>
2019-08-06 18:03:19 +0800

    Changes to be committed: 	new file:   spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/controller/EmployeeController.java 	modified:   spring-boot-04-web-restfulcrud/src/main/resources/templates/dashboard.html 	renamed:    spring-boot-04-web-restfulcrud/src/main/resources/templates/list.html -> spring-boot-04-web-restfulcrud/src/main/resources/templates/emp/list.html

---
+++ spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package cc.larryzeta.springboot.controller;
+
+import cc.larryzeta.springboot.dao.EmployeeDao;
+import cc.larryzeta.springboot.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @GetMapping("/emps")
+    public String list(Model model) {
+        Collection<Employee> employees = employeeDao.getAll();
+
+        model.addAttribute("emps", employees);
+        return "/emp/list";
+    }
+}
