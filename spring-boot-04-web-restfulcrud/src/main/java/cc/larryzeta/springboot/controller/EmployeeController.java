91dab12c08cb153823224a7688e4053f999011e4
LarryZeta <i@larryzeta.cc>
2019-08-12 18:04:23 +0800

    42 done Changes to be committed: 	modified:   spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/controller/EmployeeController.java 	modified:   spring-boot-04-web-restfulcrud/src/main/resources/templates/emp/add.html 	modified:   spring-boot-04-web-restfulcrud/src/main/resources/templates/emp/list.html

--- spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/cc/larryzeta/springboot/controller/EmployeeController.java
@@ -7,9 +7,9 @@ import cc.larryzeta.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.Collection;
 
 @Controller
@@ -43,4 +43,18 @@ public class EmployeeController {
         return "redirect:/emps";
     }
 
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
 }
